4bea4a0391a9bc9f3c6912c5b2d63429cf0cf453
KenzoCoenaerts-r0683311 <32226298+KenzoCoenaerts-r0683311@users.noreply.github.com>
2017-11-25 13:52:29 +0100

    Add files via upload
    refactored SplelerLijst klass
    moved SpelerLijst to db folder

---
+++ OOO-project/OOO_Part2/yahtzee/src/db/SpelersLijst.java
@@ -0,0 +1,32 @@
+package db;
+
+import domain.DomainException;
+import domain.Speler;
+
+import java.util.ArrayList;
+
+public class SpelersLijst {
+	private ArrayList<Speler> spelersLijst;
+
+	public SpelersLijst() {
+		spelersLijst = new ArrayList<Speler>();
+	}
+
+	public ArrayList<Speler> getSpelers() {
+		return this.spelersLijst;
+	}
+
+	public void addSpeler(Speler nieuweSpeler) {
+		spelersLijst.add(nieuweSpeler);
+	}
+
+    public boolean beschikbareSpeler(Speler nieuweSpeler) {
+        for (Speler speler : this.getSpelers()) {
+            if (speler.equals(nieuweSpeler)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
